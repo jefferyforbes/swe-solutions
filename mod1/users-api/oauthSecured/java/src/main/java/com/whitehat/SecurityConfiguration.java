7261e50c343822a03dc810e9f9eff8526758a191
Amanda Warren <amanda.warren@whitehat.org>
2020-12-22 21:04:35 +0000

    corrected javadoc

--- mod1/users-api/oauthSecured/java/src/main/java/com/whitehat/SecurityConfiguration.java
+++ mod1/users-api/oauthSecured/java/src/main/java/com/whitehat/SecurityConfiguration.java
@@ -1,22 +1,17 @@
 package com.whitehat;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.*;
 
 /**
- * Specify that all REST resources are protected by Basic Auth.
+ * Specify that all REST resources are protected by OAuth.
  */
 @Configuration
 @EnableWebSecurity
