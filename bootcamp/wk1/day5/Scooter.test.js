1773aa2a7e5a03b61ea6a358c8806c0aa5c7d030
Amanda Warren <amanda.warren@whitehat.org>
2021-01-04 07:36:07 +0000

    day5 start of solution

---
+++ bootcamp/wk1/day5/Scooter.test.js
@@ -0,0 +1,10 @@
+const Scooter = require('./Scooter');
+
+describe('Scooter', function () {
+    test('charge', function () {
+        const scooter = new Scooter();
+        scooter.charge();
+    })
+
+    // TODO - rest of tests to ensure 100% coverage
+});
