1e0e1a03aae987d237faa5e9c93579e13f9b457b
Amanda Warren <amanda.warren@whitehat.org>
2021-01-05 21:16:27 +0000

    added non-blocking callback

--- bootcamp/wk1/day5/Scooter.test.js
+++ bootcamp/wk1/day5/Scooter.test.js
@@ -1,9 +1,11 @@
 const Scooter = require('./Scooter');
 
 describe('Scooter', function () {
-    test('charge', function () {
+
+    test('charge', function (done) {
         const scooter = new Scooter();
-        scooter.charge();
+        scooter.charge(done); 
+        console.log('Charge completed');  
     })
 
     // TODO - rest of tests to ensure 100% coverage
