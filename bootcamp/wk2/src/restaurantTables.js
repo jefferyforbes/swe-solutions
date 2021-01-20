f314d5d6fa3c976d3b617b9de7aed227a2132aa3
Amanda Warren <amanda.warren@whitehat.org>
2021-01-20 00:11:07 +0000

    more db stuff

--- bootcamp/wk2/src/restaurantTables.js
+++ bootcamp/wk2/src/restaurantTables.js
@@ -3,33 +3,26 @@ const sqlite3 = require('sqlite3').verbose();
 // use a persistent database named db.sqlite
 const db = new sqlite3.Database('./restaurantdb1.sqlite');
 
-/**
- * Executes the SQL statements one at a time.
- * 
- * Note the use of try/finally to ensure resources get closed 
- * whether an error occurs or not
- * 
- */
 try {
     db.serialize(function () { // serialize means execute one statement at a time
 
         // create the empty tables with specific columns and column types
-        db.run("CREATE TABLE RESTAURANTS (id INT, name TEXT, imagelink TEXT, menuId INT)");
-        db.run("CREATE TABLE MENUS (id INT, title TEXT, menuItem INT)");
-        db.run("CREATE TABLE MENU_ITEMS (id INT, name TEXT, price REAL)");
+        db.run("CREATE TABLE RESTAURANTS (id INT PRIMARY KEY, name TEXT, imagelink TEXT, menuId INT)");
+        db.run("CREATE TABLE MENUS (id INT PRIMARY KEY, title TEXT, menuItem INT)");
+        db.run("CREATE TABLE MENU_ITEMS (id INT PRIMARY KEY, name TEXT, price REAL)");
 
         let stmt;
 
         // insert rows
-        try {           
-            stmt = db.prepare(`INSERT INTO RESTAURANTS VALUES (?, ?, ?)`);
+        try {
+            stmt = db.prepare(`INSERT INTO RESTAURANTS VALUES (?, ?, ?, ?)`);
             stmt.run(1, 'Bayroot', 'https://www.telegraph.co.uk/content/dam/Travel/Destinations/Europe/England/Brighton/brighton-restaurants-hotel-du-vin-bistro.jpg', 1);
             stmt.run(2, 'The Berkley', 'https://www.the-berkeley.co.uk/siteassets/restaurants--bars/the-garden-at-the-berkeley/the-garden-at-the-berkeley-teaser.jpg?w=620&h=560&scale=both&mode=crop', 2);
         } finally {
             // release resources 
             stmt.finalize();
         }
-        try{
+        try {
             stmt = db.prepare(`INSERT INTO MENUS VALUES (?, ?, ?)`);
             stmt.run(1, 'Afternoon tea', 1);
             stmt.run(2, 'Breakfast Menu', 2);
