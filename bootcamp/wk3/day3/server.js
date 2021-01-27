5501206811b8bd55c5384188ded392c25dbf391e
Amanda Warren <amanda.warren@whitehat.org>
2021-01-27 20:48:43 +0000

    day 3 solution

---
+++ bootcamp/wk3/day3/server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const Handlebars = require('handlebars')
+const expressHandlebars = require('express-handlebars')
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
+const { Restaurant } = require('./Restaurant')
+const { Menu } = require('./Menu')
+const { MenuItem } = require('./MenuItem')
+const { loadAndInsert } = require('./populateDB')
+
+const app = express();
+const port = 3000;
+
+// setup our templating engine
+const handlebars = expressHandlebars({
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
+})
+app.engine('handlebars', handlebars)
+app.set('view engine', 'handlebars')
+
+// serve static assets from the public/ folder
+app.use(express.static('public'));
+
+// this route matches any GET request to the http://localhost:3000
+app.get('/', async (req, res) => {
+    const restaurants = await Restaurant.findAll({
+
+        include: [
+            {
+                model: Menu, as: 'menus',
+                include: [{model:MenuItem, as: 'items'}]
+            }
+        ],
+        nest: true
+    })
+    res.render('home', { restaurants })
+})
+
+app.get('/restaurants/:id', async (req, res) => {
+    const restaurant = await Restaurant.findByPk(req.params.id)
+    const menus = await restaurant.getMenus({
+        include: [{model: MenuItem, as: 'items'}],
+        nest: true
+    })
+    res.render('restaurant', {restaurant, menus})
+})
+
+app.get('/items/:id', async (req, res) => {
+    console.log('Got here!');
+    const menus = await Menu.findByPK(req.params.id);
+    const menuItems = await menus.getItems({
+        include: [{model: MenuItem, as }], 
+        nest: true 
+    });
+    res.render('items')
+})
+
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`)
+})
