ee1d5a72157cd4ef888ab9ea3f3e3d921d076151
Daniel Miller <millerpils@gmail.com>
2021-02-09 16:59:33 +0000

    Nicer sessions solution

--- mod1/sessions/solution/controllers/login.js
+++ mod1/sessions/solution/controllers/login.js
@@ -1,29 +1,10 @@
-const bcrypt = require("bcrypt");
-const User = require("../models/user");
-
 module.exports = (req, res) => {
-  const { username, password } = req.body;
+  if (!req.session.userId) {
+    // use mongo user id as session id
+    req.session.userId = `${req.auth.user}123!`;
+  }
 
-  User.findOne({ username: username }, (error, user) => {
-    if (!user) {
-      return res
-        .status(404)
-        .send(`Couldn't find user with username: ${username}`);
-    }
-
-    bcrypt.compare(password, user.password, (error, same) => {
-      if (!same)
-        return res.status(401).send("Sorry, your password was incorrect.");
-
-      if (same) {
-        // use mongo user id as session id
-        req.session.userId = user._id;
-        res
-          .status(200)
-          .send(
-            `You're logged in, ${user.username}! Now call other API routes.`
-          );
-      }
-    });
-  });
+  res
+    .status(200)
+    .send(`You're logged in, ${req.auth.user}! Now call other API routes.`);
 };
