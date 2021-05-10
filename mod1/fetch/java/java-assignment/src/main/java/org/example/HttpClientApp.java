74d7270d045cc5f3a84da3c90d8c3e5c32f5e1bd
Amanda Warren <amanda.warren@whitehat.org>
2021-05-10 18:15:45 +0100

    nice refactorings

--- mod1/fetch/java/java-assignment/src/main/java/org/example/HttpClientApp.java
+++ mod1/fetch/java/java-assignment/src/main/java/org/example/HttpClientApp.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpHeaders;
 import java.net.http.HttpRequest;
@@ -14,43 +15,118 @@ import java.util.Map;
  * we have chosen to use java.net.http.HttpClient as it is nice and easy!
  */
 public class HttpClientApp {
+
+    private static final java.net.http.HttpClient client = java.net.http.HttpClient.newHttpClient();
+    private static final String url = "https://http-challenge.multiverse-coaches.io/";
+
+    public HttpResponse getRequest() throws IOException, InterruptedException {
+
+        HttpRequest request = HttpRequest.newBuilder(
+                URI.create(url))
+                .header("accept", "application/json")
+                .build();
+
+        // TODO - work out best way to print the raw request
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+        return response;
+    }
+
+    public HttpResponse postRequest()  throws IOException, InterruptedException {
+
+        String jsonName = "{" +
+                "\"name\":\"mandy\"" +
+                "}";
+
+        HttpRequest request = HttpRequest.newBuilder(
+                URI.create(url+"apprentices"))
+                .header("Content-Type", "application/json")
+                .header("accept", "text/plain; text/html; /*/")
+                .method("POST", HttpRequest.BodyPublishers.ofString(jsonName))
+                .build();
+
+        HttpResponse<String>  response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+        return response;
+    }
+
+    public HttpResponse headerRequest(String headerValue)  throws IOException, InterruptedException {
+        // third request (header)
+        HttpRequest request = HttpRequest.newBuilder(
+                URI.create(url+"apprentices/"+headerValue))
+                .header("Content-Type", "application/json")
+                .headers("your-id", headerValue)
+                .header("accept", "text/plain; text/html; /*/")
+                .build();
+
+        HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+        return response;
+    }
+
+    public HttpResponse patchRequest(String headerValue)  throws IOException, InterruptedException {
+
+        String urlEncodedName = "guests=fred%2Cbarny%2Cwilma";
+
+        HttpRequest request = HttpRequest.newBuilder(
+                URI.create(url+"apprentices/"+headerValue))
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .headers("your-id", headerValue)
+                .header("accept", "text/plain; text/html; /*/") //
+                .method("PATCH", HttpRequest.BodyPublishers.ofString(urlEncodedName))
+                .build();
+
+        HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+        return response;
+    }
+
+    public HttpResponse queryRequest(String headerValue)  throws IOException, InterruptedException {
+
+        String menuQueryParams = "starter=prawns&main=roast%20chicken&dessert=banana%20split";
+
+        HttpRequest request = HttpRequest.newBuilder(
+                URI.create(url+"apprentices/"+headerValue+"/menus?"+menuQueryParams))
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .headers("your-id", headerValue)
+                .header("accept", "text/plain; text/html; /*/") //
+                .build();
+
+        HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+        return response;
+    }
+
+    public HttpResponse deleteRequest(String headerValue)  throws IOException, InterruptedException {
+
+        HttpRequest request = HttpRequest.newBuilder(
+                URI.create(url+"apprentices/"+headerValue))
+                .method("DELETE", HttpRequest.BodyPublishers.noBody())
+                .headers("your-id", headerValue)
+                .build();
+
+        HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+        return response;
+    }
+
     public static void main( String[] args ) {
 
-        String url = "https://http-challenge.multiverse-coaches.io/";
+        HttpClientApp client = new HttpClientApp();
 
         try {
-            java.net.http.HttpClient client = java.net.http.HttpClient.newHttpClient();
+            HttpResponse getResponse = client.getRequest();
+            HttpResponse postResponse = client.postRequest();
 
-            // first request (GET)
-            HttpRequest request = HttpRequest.newBuilder(
-                    URI.create(url))
-                    .header("accept", "application/json")
-                    .build();
-
-            // TODO - work out best way to print the raw request
-            
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println(response.body());
-
-            // second request (POST)
-            String jsonName = "{" +
-                    "\"name\":\"mandy\"" +
-                    "}";
-
-            request = HttpRequest.newBuilder(
-                    URI.create(url+"apprentices"))
-                    .header("Content-Type", "application/json")
-                    .header("accept", "text/plain; text/html; /*/")
-                    .method("POST", HttpRequest.BodyPublishers.ofString(jsonName))
-                    .build();
-
-            System.out.println(request);
-
-            response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println(response.body());
-
-            HttpHeaders headers = response.headers();
-
+            // retrieve the value of the "your-id" header
+            HttpHeaders headers = postResponse.headers();
             String headerValue = null;
 
             Map<String, List<String>> map = headers.map();
@@ -62,6 +138,11 @@ public class HttpClientApp {
                 }
             }
 
+            HttpResponse headerResponse = client.headerRequest(headerValue);
+            HttpResponse patchResponse = client.patchRequest(headerValue);
+            HttpResponse queryResponse = client.queryRequest(headerValue);
+            HttpResponse deleteResponse = client.deleteRequest(headerValue);
+
             /* // Lambda alternative for header retrieval
             List<String> val = headers.map().entrySet().stream()
                     .filter(entry -> entry.getKey().equalsIgnoreCase("your-id"))
@@ -71,45 +152,6 @@ public class HttpClientApp {
                     .findFirst()
                     .orElse(null);
                     */
-
-            // third request (header)
-            request = HttpRequest.newBuilder(
-                    URI.create(url+"apprentices/7b37ba22"))
-                    .header("Content-Type", "application/json")
-                    .headers("your-id", headerValue)
-                    .header("accept", "text/plain; text/html; /*/")
-                    .build();
-
-            response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println(response.body());
-
-            // forth request (PATCH)
-            String urlCodedName = "guests=fred%2Cbarny%2Cwilma";
-
-            request = HttpRequest.newBuilder(
-                    URI.create(url+"apprentices/7b37ba22"))
-                    .header("Content-Type", "application/x-www-form-urlencoded")
-                    .headers("your-id", headerValue)
-                    .header("accept", "text/plain; text/html; /*/") //
-                    .method("PATCH", HttpRequest.BodyPublishers.ofString(urlCodedName))
-                    .build();
-
-            response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println(response.body());
-
-            // fifth request (GET with Query Parameters)
-            String menuQueryParams = "starter=prawns&main=roast%20chicken&dessert=banana%20split";
-
-            request = HttpRequest.newBuilder(
-                    URI.create(url+"apprentices/"+headerValue+"?"+menuQueryParams))
-                    .header("Content-Type", "application/x-www-form-urlencoded")
-                    .headers("your-id", headerValue)
-                    .header("accept", "text/plain; text/html; /*/") //
-                    .build();
-
-            response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println(response.body());
-
         } catch (Exception e) {
             e.printStackTrace();
         }
