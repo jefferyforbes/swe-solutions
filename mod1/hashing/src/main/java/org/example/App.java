96a2bb5200d45bf79b387df824977e0680c94bda
Amanda Warren <amanda.warren@whitehat.org>
2021-05-27 19:15:06 +0100

    more solutions

---
+++ mod1/hashing/src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String hashedPassword = passwordEncoder.encode("your password");
+
+        System.out.println(hashedPassword );
+
+        boolean isMatch = passwordEncoder.matches("your password", "$2b$10$AQXoVkfzAovJ9RHTtmd6N.Yegy3V9ALTlYDcCM76HxBqq044q6xLK");
+    }
+}
