4d94bce3bbcc0817d368138a2d637bb30c5fa036
Charlie Merrell <charlie.merrell20@gmail.com>
2021-07-01 10:24:36 +0100

    adds remove button to menu items

--- bootcamp/Unit-5-HCI/documentObjectModel/public/populateRestaurant.js
+++ bootcamp/Unit-5-HCI/documentObjectModel/public/populateRestaurant.js
@@ -1,10 +1,10 @@
-const restaurantId = document.querySelector('h1').id.split('-')[1];
-fetch('/restaurants/' + restaurantId)
-    .then(res => res.json())
+const restaurantId = document.querySelector("h1").id.split("-")[1];
+fetch("/restaurants/" + restaurantId)
+    .then((res) => res.json())
     .then(renderRestaurantData);
 
 function renderRestaurantData(restaurant) {
-    const menuSection = document.getElementById('menus');
+    const menuSection = document.getElementById("menus");
     for (const menu of restaurant.Menus) {
         menuSection.append(makeMenuEl(menu));
     }
@@ -16,7 +16,7 @@ function makeMenuEl(menu) {
     menuTitle.innerText = menu.title;
     menuEl.append(menuTitle);
     for (const item of menu.MenuItems) {
-        menuEl.append(makeItemEl(item))
+        menuEl.append(makeItemEl(item));
     }
     return menuEl;
 }
@@ -24,7 +24,20 @@ function makeMenuEl(menu) {
 function makeItemEl(item) {
     const itemEl = document.createElement("li");
     const text = `${item.name} - £${item.price}`;
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "delete";
+    deleteButton.addEventListener(
+        "click",
+        async () => {
+            itemEl.remove();
+            await fetch(`/items/${item.id}`, { method: "DELETE" });
+            // in a real app, you'd show a loading spinner whilst waiting
+        },
+        { once: true } // removes itself after first use
+    );
     itemEl.innerText = text;
+    itemEl.append(deleteButton);
+
     return itemEl;
 }
 
