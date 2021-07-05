77009155deabc14b9c4fdc87a743cbb613a27d2a
Daniel Miller <millerpils@gmail.com>
2021-07-05 14:58:53 +0100

    Add mod1/plants direct

--- bootcamp/Unit-1-Object_Oriented_Programming/Objects_in_an_Airport_Domain/sample/Airport.test.js
+++ bootcamp/Unit-1-Object_Oriented_Programming/Objects_in_an_Airport_Domain/sample/Airport.test.js
@@ -4,32 +4,31 @@ const Passenger = require('./Passenger');
 const Bag = require('./Bag');
 
 describe('Airport', () => {
-    test('has a name', () => {
-        const LHR = new Airport('LHR');
-        expect(LHR.name).toBe('LHR');
-    });
+  test('has a name', () => {
+    const LHR = new Airport('LHR');
+    expect(LHR.name).toBe('LHR');
+  });
 
+  test('an airport has planes', () => {
+    const plane1 = new Plane();
+    plane1.setOrigin('LHR');
+    plane1.setDestination('LAX');
 
-    test('an airport has planes', () => {
-        const plane1 = new Plane();
-        plane1.setOrigin('LHR');
-        plane1.setDestination('LAX');
+    const pax1 = new Passenger({ name: 'fred' });
+    const cabinLuggage = new Bag(8);
+    const holdLuggage = new Bag(25);
+    pax1.addBag(cabinLuggage);
+    pax1.addBag(holdLuggage);
 
-        const pax1 = new Passenger({name: "fred"});
-        const cabinLuggage = new Bag(8);
-        const holdLuggage = new Bag(25);
-        pax1.addBag(cabinLuggage);
-        pax1.addBag(holdLuggage);
+    plane1.boardPassenger(pax1);
 
-        plane1.boardPassenger(pax1);
+    const LHR = new Airport('LHR');
+    LHR.addPlane(plane1);
 
-        const LHR = new Airport('LHR');
-        LHR.addPlane(plane1);
+    expect(LHR.planes.length).toBe(1);
 
-        expect(LHR.planes.length).toBe(1);
-
-        expect(LHR.planes[0].passengers.length).toBe(1);
-        expect(LHR.planes[0].passengers[0]).toMatchObject(pax1);
-        expect(LHR.planes[0].passengers[0].bags[0].weight).toBe(8);
-    });
-})
+    expect(LHR.planes[0].passengers.length).toBe(1);
+    expect(LHR.planes[0].passengers[0]).toMatchObject(pax1);
+    expect(LHR.planes[0].passengers[0].bags[0].weight).toBe(8);
+  });
+});
