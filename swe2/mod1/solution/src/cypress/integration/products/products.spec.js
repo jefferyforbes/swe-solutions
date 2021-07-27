4645d3077d1c3b555a4454d79a0885416856dfe0
Daniel Miller <millerpils@gmail.com>
2021-07-27 17:17:11 +0100

    Cypress bits

--- swe2/mod1/solution/src/cypress/integration/products/products.spec.js
+++ swe2/mod1/solution/src/cypress/integration/products/products.spec.js
@@ -1,13 +1,22 @@
 describe('Products', () => {
-  before(() => {
+  beforeEach(() => {
     cy.visit('/');
   });
 
-  it('changes the button text on add to cart', () => {
+  it('should change the button text when clicked', () => {
     cy.addAllProductsToCart();
 
     cy.get('[data-cy=addToCartBtn]').each(($el, index) => {
       expect($el).to.contain('Remove from cart');
     });
+  });
+
+  it('should navigate to first product details page', () => {
+    cy.get('[data-cy=product-link]')
+      .eq(0)
+      .then(($el, index) => {
+        cy.visit($el[0].href);
+        cy.url().should('include', $el[0].hash);
+      });
   });
 });
