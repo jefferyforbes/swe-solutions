84b0cbe76a7a85ad1ab28ace2085505680b07eff
Amanda Warren <amanda.warren@whitehat.org>
2021-09-28 22:08:03 +0100

    updated js soln

--- swe2/mod2/passwordHashing/js/hash.js
+++ swe2/mod2/passwordHashing/js/hash.js
@@ -2,3 +2,6 @@ const bcrypt = require('bcrypt')
 
 // hash the password 'password101'
 bcrypt.hash('password101', 10).then(console.log)
+
+// compare a password with a hash
+bcrypt.compare('password101', '$2b$10$AQXoVkfzAovJ9RHTtmd6N.Yegy3V9ALTlYDcCM76HxBqq044q6xLK').then(console.log)
