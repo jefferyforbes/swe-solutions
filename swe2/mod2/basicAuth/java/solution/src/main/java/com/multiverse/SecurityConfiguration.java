e55d8de3c6d6970590c1eb341a9eb1a63a10c377
Amanda Warren <amanda.warren@whitehat.org>
2021-09-29 07:41:26 +0100

    java solution for basic auth

---
+++ swe2/mod2/basicAuth/java/solution/src/main/java/com/multiverse/SecurityConfiguration.java
@@ -0,0 +1,52 @@
+package com.multiverse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+/**
+ * Specify that all REST resources are protected by Basic Auth.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        // ** IMPORTANT! do not use the line below in production apps!! **
+        httpSecurity.csrf().disable(); // hack to support DELETE method
+        httpSecurity.authorizeRequests().anyRequest().authenticated()
+                .and().httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authentication)
+            throws Exception {
+        // use jdbc authentication (for in memory authentication use authentication.inMemoryAuthentication())
+        authentication.jdbcAuthentication()
+                .dataSource(dataSource)
+                .authoritiesByUsernameQuery("select username,authority "
+                        + "from authorities "
+                        + "where username = ?")
+                .usersByUsernameQuery(
+                        "select username, password, 'true' as enabled from users where username = ?");
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // specify we want the password hashed using bcrypt
+        return new BCryptPasswordEncoder();
+    }
+}
